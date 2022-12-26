81fa842f92cabfd574e683d676f636b08cb74234
tundekaz01 <117520897+tundekaz01@users.noreply.github.com>
2022-12-26 17:40:39 +0000

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-0ee23bfc74a881de5"
-      key_name = var.key_name
+      ami           = "ami-061dbd1209944525c"
+      key_name = "myJenkinsKey"
       instance_type = "t2.micro"
       vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
       tags= {
